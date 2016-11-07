66bffb7c39d66b4f16ef09c22988a4ef8d19dacd
zulfikra21 <zulfikralahmudin@gmail.com>
2016-11-07 17:17:22 +0700

    fixed

--- controllers/home.php
+++ controllers/home.php
@@ -14,31 +14,12 @@ class home extends controller{
 	{
 
 
-		//$assocText	=	$postingan->fetch(PDO::FETCH_ASSOC);
+
 		return $this->view('home/index');
 
-		// return $this->view('home/index');
+
 
 	}
-
-	public function delete($id)
-	{
-		$post = new post;
-		$post->deletePost($id);
-	}
-
-	public function detail($id)
-	{
-		require 'app/connect.php';
-		$selectDb = $db->query("SELECT * FROM postingan WHERE Judul='$id'");
-
-		$selectAssoc  = $selectDb->fetch(PDO::FETCH_ASSOC);
-
-		$postingan	  =  htmlspecialchars_decode($selectAssoc['postingan'],ENT_QUOTES);
-
-		 return $this->view('detailPage/index',['posting' => $selectAssoc, 'postingan' => $postingan]);
-	}
-
 
 
 
