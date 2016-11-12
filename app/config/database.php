61efca022e498311d714dc8ef8e767c8dd239b8a
zulfikra21 <zulfikralahmudin@gmail.com>
2016-11-12 23:40:32 +0700

    fixed

--- app/config/database.php
+++ app/config/database.php
@@ -1,5 +1,11 @@
 <?php
 //definisikan  database dibawah ini,
+//host adalah hosting yang anda gunakan contoh localhost, 127.0.0.1 atau host yang anda gunakan
+//diwebsite anda
+//dbname adalah nama database anda
+//username adalah username yang anda gunakan
+//begitu juga password
+
 define('host','');
 define('dbName','');
 define('username','');
